6fbce8a6a8125cc6ebf701263f3852038057ae59
Sabrina Sides <sabrinasides91@gmail.com>
2021-05-13 09:51:20 -0500

    adding new project

--- src/Projects/ProjectsPage.js
+++ src/Projects/ProjectsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Project from './Project';
 import NPProject from '../images/NPProject.jpg';
+import Noteful from '../images/Noteful.jpeg';
 import './ProjectsPage.css';
 
 export default function Projects() {
@@ -14,9 +15,17 @@ export default function Projects() {
           src={NPProject}
           description="This app utilizes multiple APIs to help users find which national
           parks support their favorites outdoor activities."
-          skills="HTML, CSS, Javascript, jQuery"
+          skills="HTML, CSS, JavaScript, jQuery"
           urlRepo="https://github.com/SabrinaSides/NationalParksActivitySearch"
           urlLive="https://sabrinasides.github.io/NationalParksActivitySearch"
+        />
+        <hr />
+        <Project
+          title="Noteful"
+          src={Noteful}
+          description="Created in coordination with the Thinkful curriculum, this app allows users to create and organize notes."
+          skills="HTML, CSS, JavaScript, React"
+          urlRepo="https://github.com/SabrinaSides/noteful-client"
         />
       </div>
     </div>
